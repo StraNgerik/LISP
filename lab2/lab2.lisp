5cb4bb9ce8bedbea4974f9b25bf66d8c2db13683
Bychko Kirilo <109760434+StraNgerik@users.noreply.github.com>
2024-10-12 18:38:12 +0300

    Add files via upload

--- lab2/lab2.lisp
+++ lab2/lab2.lisp
@@ -25,26 +25,26 @@
 (check-remove-seconds-and-thirds "Task 1 test 6" '(1 a g d 5) '(1 d))
 
 
-(defun list-set-intersection (a b bcons)
-  (if (null a)
-      nil
-    (if (null b)
-        (list-set-intersection (cdr a) bcons bcons)
-      (if (= (car a) (car b))
-          (cons (car a) (list-set-intersection (cdr a) bcons bcons))
-        (list-set-intersection a (cdr b) bcons)
-      )
-    )
-  )
-)
+(defun list-set-intersection (a b)
+  (unless (null a)
+    (let ((x (car a)))
+      (if (find-in-list x b)
+          (cons x (list-set-intersection (cdr a) b))
+        (list-set-intersection (cdr a) b)))))
+
+(defun find-in-list (x lst)
+  (cond ((null lst) nil)
+        ((equal x (car lst)) t)
+        (t (find-in-list x (cdr lst)))))
 
 (defun check-list-set-intersection (name a b expected)
   (format t "~:[FAILED~;passed~]... ~a~%"
-    (equal (list-set-intersection a b b) expected)
+    (equal (list-set-intersection a b) expected)
     name)
 )
 
-(check-list-set-intersection "Task 2 test1" '(3 4 2 7) '(3 4 5 6) '(3 4))
-(check-list-set-intersection "Task 2 test2" '(1 2 3 4) '(4 5 6 7) '(4))
-(check-list-set-intersection "Task 2 test3" '(5 6 7 3) '(3 7 4 3) '(7 3))
-(check-list-set-intersection "Task 2 test4" '(9 2 3 1) '(1 2 3 9) '(9 2 3 1))
+(check-list-set-intersection "test1" '(3 4 2 7) '(3 4 5 6) '(3 4))
+(check-list-set-intersection "test2" '(1 2 3 4) '(4 5 6 7) '(4))
+(check-list-set-intersection "test3" '(5 6 7 3) '(3 7 4 3) '(7 3))
+(check-list-set-intersection "test4" '(9 2 3 1) '(1 2 3 9) '(9 2 3 1))
+(check-list-set-intersection "test5" '(A B C D) '(C D E F) '(C D))
