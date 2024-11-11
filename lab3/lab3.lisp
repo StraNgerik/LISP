639d21f1d1ccaa13f3cc104badbfd2721ebb7bc4
Bychko Kirilo <109760434+StraNgerik@users.noreply.github.com>
2024-11-11 15:13:50 +0200

    Update lab3.lisp

--- lab3/lab3.lisp
+++ lab3/lab3.lisp
@@ -1,32 +1,26 @@
-(defun bubble-sort-functional-loop-in (A R i)
-  (if (>= i (- R 1))
-      A
-      (bubble-sort-functional-loop-in
-       (if (> (nth i A) (nth (1+ i) A))
-           (append (subseq A 0 i)
-                   (list (nth (1+ i) A))
-                   (list (nth i A))
-                   (nthcdr (+ 2 i) A))
-           A)
-       R 
-       (+ i 1))))
+(defun bubble-sort-pass (A)
+    (if (null (cdr A))
+      A      (let ((rest (bubble-sort-pass (cdr A))))
+        (if (> (car A) (car rest))            (cons (car rest) (cons (car A) (cdr rest)))
+            (cons (car A) rest)))))
 
-(defun bubble-sort-functional (A len)
-    (if (<= len 1)
-        A
-        (bubble-sort-functional
-         (bubble-sort-functional-loop-in A len 0) (- len 1))))
+(defun bubble-sort-functional-rec (A R)
+    (if (<= R 1)
+      A      (bubble-sort-functional-rec (bubble-sort-pass A) (- R 1))))
 
-(defun check-bubble_sort_funtional (name input len expected)
+(defun bubble-sort-functional (A)
+  (let ((len (length A)))    (bubble-sort-functional-rec A len)))
+
+(defun check-bubble_sort_funtional (name input expected)
 (format t "~:[FAILED~;passed~]... ~a~%"
-(equal (bubble-sort-functional input len) expected)
+(equal (bubble-sort-functional input) expected)
 name))
 
-(check-bubble_sort_funtional "Functional Test 1 " '(4 8 1 2 5) '5 '(1 2 4 5 8))
-(check-bubble_sort_funtional "Functional Test 2 " '(1 3 5 7 9) '5 '(1 3 5 7 9))
-(check-bubble_sort_funtional "Functional Test 3 " '(9 7 5 3 1) '5 '(1 3 5 7 9))
-(check-bubble_sort_funtional "Functional Test 4 " '(2 2 2 2 2) '5 '(2 2 2 2 2))
-(check-bubble_sort_funtional "Functional Test 5 " '(10 -3 25 0 7 15) '6 '(-3 0 7 10 15 25))
+(check-bubble_sort_funtional "Functional Test 1 " '(4 8 1 2 5) '(1 2 4 5 8))
+(check-bubble_sort_funtional "Functional Test 2 " '(1 3 5 7 9) '(1 3 5 7 9))
+(check-bubble_sort_funtional "Functional Test 3 " '(9 7 5 3 1) '(1 3 5 7 9))
+(check-bubble_sort_funtional "Functional Test 4 " '(2 2 2 2 2) '(2 2 2 2 2))
+(check-bubble_sort_funtional "Functional Test 5 " '(10 -3 25 0 7 15) '(-3 0 7 10 15 25))
 
 (defun bubble-sort-imperative (A)
   (let ((Answ (copy-list A)) (n (list-length A)))
